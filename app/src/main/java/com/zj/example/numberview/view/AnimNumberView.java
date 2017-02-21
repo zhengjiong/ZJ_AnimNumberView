733d9ba2106449e0a0bc9576f1cc3254b84ddd58
zhengjiong <zhengjiong2006@gmail.com>
2017-02-21 10:36:19 +0800

    finish animnumberview

--- app/src/main/java/com/zj/example/numberview/view/AnimNumberView.java
+++ app/src/main/java/com/zj/example/numberview/view/AnimNumberView.java
@@ -1,7 +1,9 @@
 package com.zj.example.numberview.view;
 
+import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.text.TextUtils;
 import android.util.AttributeSet;
@@ -22,12 +24,18 @@ import com.zj.example.numberview.R;
  * @author 郑炯
  * @version 1.0
  */
-public class AnimNumberView extends LinearLayout implements View.OnClickListener {
+public class AnimNumberView extends LinearLayout implements View.OnClickListener, Animator.AnimatorListener {
+    public static final int DURATION = 400;
     private ImageView mImgAdd;
     private ImageView mImgMin;
     private EditText mEditText;
 
     private ObjectAnimator mAddRightAnim;
+    private ObjectAnimator mAddLeftAnim;
+    private ObjectAnimator mMinLeftAnim;
+    private ObjectAnimator mMinRightAnim;
+    private ObjectAnimator mEditShowAnim;
+    private ObjectAnimator mEditHideAnim;
 
     public AnimNumberView(Context context) {
         this(context, null);
@@ -46,17 +54,46 @@ public class AnimNumberView extends LinearLayout implements View.OnClickListener
 
         mImgAdd.setOnClickListener(this);
         mImgMin.setOnClickListener(this);
-
-
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        PropertyValuesHolder rotationValuesHolder = PropertyValuesHolder.ofFloat("rotation", 0, 360);
-        PropertyValuesHolder translationXValuesHolder = PropertyValuesHolder.ofFloat("translationX", 0, w / 2 - mImgAdd.getMeasuredWidth() / 2);
-        mAddRightAnim = ObjectAnimator.ofPropertyValuesHolder(mImgAdd, rotationValuesHolder, translationXValuesHolder);
-        mAddRightAnim.setDuration(2000);
+        initAnim(w, h);
+    }
+
+    private void initAnim(int w, int h) {
+        PropertyValuesHolder positiveRotationValuesHolder = PropertyValuesHolder.ofFloat("rotation", 0, 360);
+        PropertyValuesHolder negativeRotationLeftValuesHolder = PropertyValuesHolder.ofFloat("rotation", 0, -360);
+        PropertyValuesHolder addTranslationXRightValuesHolder = PropertyValuesHolder.ofFloat("translationX", 0, w / 2 - mImgAdd.getMeasuredWidth() / 2);
+        PropertyValuesHolder addTranslationXLeftValuesHolder = PropertyValuesHolder.ofFloat("translationX", w / 2 - mImgAdd.getMeasuredWidth() / 2, 0);
+
+        PropertyValuesHolder minTranslationXLeftValuesHolder = PropertyValuesHolder.ofFloat("translationX", 0, -1 * (w / 2 - mImgAdd.getMeasuredWidth() / 2));
+        PropertyValuesHolder minTranslationXRightValuesHolder = PropertyValuesHolder.ofFloat("translationX", -1 * (w / 2 - mImgAdd.getMeasuredWidth() / 2), 0);
+
+        mAddRightAnim = ObjectAnimator.ofPropertyValuesHolder(mImgAdd, positiveRotationValuesHolder, addTranslationXRightValuesHolder);
+        mAddRightAnim.setDuration(DURATION);
+        mAddRightAnim.addListener(this);
+
+        mAddLeftAnim = ObjectAnimator.ofPropertyValuesHolder(mImgAdd, negativeRotationLeftValuesHolder, addTranslationXLeftValuesHolder);
+        mAddLeftAnim.setDuration(DURATION);
+        mAddLeftAnim.addListener(this);
+
+        mMinLeftAnim = ObjectAnimator.ofPropertyValuesHolder(mImgMin, negativeRotationLeftValuesHolder, minTranslationXLeftValuesHolder);
+        mMinLeftAnim.setDuration(DURATION);
+        mMinLeftAnim.addListener(this);
+
+        mMinRightAnim = ObjectAnimator.ofPropertyValuesHolder(mImgMin, positiveRotationValuesHolder, minTranslationXRightValuesHolder);
+        mMinRightAnim.setDuration(DURATION);
+        mMinRightAnim.addListener(this);
+
+        mEditShowAnim = ObjectAnimator.ofFloat(mEditText, "alpha", 0, 1f);
+        mEditShowAnim.setDuration(DURATION);
+        mEditShowAnim.addListener(this);
+
+        mEditHideAnim = ObjectAnimator.ofFloat(mEditText, "alpha", 1f, 0);
+        mEditHideAnim.setDuration(DURATION);
+        mEditHideAnim.addListener(this);
     }
 
     @Override
@@ -68,12 +105,19 @@ public class AnimNumberView extends LinearLayout implements View.OnClickListener
                 mEditText.setText(String.valueOf(count));
                 if (count == 1) {
                     mAddRightAnim.start();
+                    mImgMin.setVisibility(View.VISIBLE);
+                    mEditText.setVisibility(View.VISIBLE);
+                    mMinLeftAnim.start();
+                    mEditShowAnim.start();
                 }
                 break;
             case R.id.btn_min:
                 count--;
                 if (count <= 0) {
                     count = 0;
+                    mEditHideAnim.start();
+                    mMinRightAnim.start();
+                    mAddLeftAnim.start();
                 }
                 mEditText.setText(String.valueOf(count));
                 break;
@@ -86,5 +130,31 @@ public class AnimNumberView extends LinearLayout implements View.OnClickListener
         } else {
             return 0;
         }
+    }
+
+    @Override
+    public void onAnimationStart(Animator animation) {
+        setEnable(false);
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animation) {
+        setEnable(true);
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animation) {
+        setEnable(true);
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animation) {
+
+    }
+
+    private void setEnable(boolean enabled) {
+        mImgAdd.setEnabled(enabled);
+        mImgMin.setEnabled(enabled);
+        mEditText.setEnabled(enabled);
     }
 }
